b9335df5878f5bb476f2e2afed14dae1cb8404b3
begoat <hylbegoat@gmail.com>
2020-04-05 14:30:35 +0800

    feat(markdown-plugin): read markdown and render it in the url matching file path

--- blogs/gatsby-node.js
+++ blogs/gatsby-node.js
@@ -1,7 +1,49 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const _ = require('lodash');
+const { createFilePath } = require('gatsby-source-filesystem');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({ actions: { createPage }, graphql }) => {
+  const blogPost = require.resolve('./src/templates/blog-post.js');
+  const results = await graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              title
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = _.get(results, 'data.allMarkdownRemark.edges', []);
+  posts.forEach(post => {
+    const { slug } = post.node.fields;
+    console.log('slug', [slug]);
+    createPage({
+      path: slug,
+      component: blogPost,
+      context: {
+        slug,
+        previous: 'preserved',
+        next: 'preserved',
+      }
+    });
+  });
+};
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+  if (_.get(node, 'internal.type') === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+};
---
+++ blogs/src/templates/blog-post.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+const BlogPostTmpl = ({ data }) => {
+  const { html } = data.markdownRemark;
+  return (
+    <section>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </section>
+  );
+};
+
+export default BlogPostTmpl;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      html
+      timeToRead
+      frontmatter {
+        title
+      }
+      fields {
+        slug
+      }
+    }
+  }
+`;
