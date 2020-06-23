c2c9cd90a50f626d3190e3edbc4eb3be961ba21a
begoat <hylbegoat@gmail.com>
2020-06-23 09:10:43 +0800

    feat(stack-and-queue/clone-graph): finish task

--- leetcode/src/learn/queue-stack/stack-and-dfs/clone-graph/index.js
+++ leetcode/src/learn/queue-stack/stack-and-dfs/clone-graph/index.js
@@ -6,10 +6,32 @@
  * };
  */
 
+export function Node(val, neighbors) {
+  this.val = val === undefined ? 0 : val;
+  this.neighbors = neighbors === undefined ? [] : neighbors;
+};
+
 /**
  * @param {Node} node
  * @return {Node}
  */
-var cloneGraph = function(node) {
-    
+export var cloneGraph = function(node) {
+  if (!node) return null;
+  const hashMap = new Map();
+  let mainStack = [node].slice();
+  hashMap.set(node, new Node(node.val));
+  while (mainStack.length > 0) {
+    const lastNode = mainStack.pop();
+    lastNode.neighbors.forEach(ne => {
+      if (!hashMap.get(ne)) {
+        hashMap.set(ne, new Node(ne.val));
+        mainStack.push(ne);
+      }
+
+      hashMap.get(lastNode).neighbors.push(hashMap.get(ne));
+    });
+
+  }
+
+  return hashMap.get(node);
 };
--- leetcode/src/learn/queue-stack/stack-and-dfs/clone-graph/index.spec.js
+++ leetcode/src/learn/queue-stack/stack-and-dfs/clone-graph/index.spec.js
@@ -1,20 +1,19 @@
 import { assert } from 'chai';
 
-import { cloneGraph } from './index';
-
-const testcase = [
-  [2324, true],
-];
+import { cloneGraph, Node } from './index';
 
 describe('cloneGraph', function() {
   it ('always true', () => {
     assert.equal(true, true);
   });
 
-  testcase.map(t => {
-    const [param, result] = t;
-    return it(param.toString(), () => {
-      assert.deepEqual(cloneGraph(param), result);
-    });
+  it('example3', () => {
+    const node1 = new Node(1);
+    const node2 = new Node(2);
+    node1.neighbors.push(node2);
+    node2.neighbors.push(node1);
+    const result = cloneGraph(node1);
+    assert.equal(result.val, 1);
+    assert.equal(result.neighbors[0].val, 2);
   });
 });
